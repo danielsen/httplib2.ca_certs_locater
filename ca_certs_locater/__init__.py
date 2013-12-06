6d5abaa4814978503870c652841dc2fb47540f48
danielsen <dnielsen@reachmail.com>
2013-12-05 20:35:11 -0600

    Added license information

--- ca_certs_locater/__init__.py
+++ ca_certs_locater/__init__.py
@@ -2,6 +2,22 @@
 # certificate authority file(s) from the base OS rather than the httplib2
 # package. Failing to find he OS-specific file, the httplib2 file will
 # be used.
+#
+# Copyright (c) 2013 Dan Nielsen (dnielsen@reachmail.com) 
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import ctypes.util
 import collections
